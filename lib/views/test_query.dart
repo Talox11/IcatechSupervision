08881e702da818fefc7540d2cacf1ebe0912e938
Talox11 <taloboti@gmail.com>
2022-03-16 15:52:03 -0600

    background service to check connection done

--- lib/views/test_query.dart
+++ lib/views/test_query.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_banking_app/repo/repository.dart';
 import 'package:flutter_banking_app/utils/layouts.dart';
 
@@ -23,6 +26,11 @@ class TestQuery extends StatefulWidget {
 class _TestQueryState extends State<TestQuery> {
   late List _futureGrupo;
   late List _listAlumnos = [];
+
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   Future<List> _getInfoGrupo() async {
     final response =
         await http.get(Uri.parse('http://10.0.2.2:5000/list/grupo'));
@@ -41,8 +49,39 @@ class _TestQueryState extends State<TestQuery> {
   @override
   void initState() {
     super.initState();
-    _getInfoGrupo();
+    // _getInfoGrupo();
+    checkInternetConnection();
+
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     // testQuery();
+  }
+
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print('Couldn\'t check connectivity status');
+      return;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
   }
 
   @override
@@ -95,7 +134,7 @@ class _TestQueryState extends State<TestQuery> {
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(100),
                         color: Colors.transparent),
-                    child: Text('Expenses',
+                    child: Text('Expenses ${_connectionStatus.toString()}',
                         style: TextStyle(
                             color: Repository.titleColor(context),
                             fontSize: 17,
@@ -108,6 +147,18 @@ class _TestQueryState extends State<TestQuery> {
         ],
       ),
     );
+  }
+}
+
+void checkInternetConnection() async {
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  if (connectivityResult == ConnectivityResult.mobile) {
+    print('connected to internet mobile');
+  } else if (connectivityResult == ConnectivityResult.wifi) {
+    print('connected to internet wifi');
+    // I am connected to a wifi network.
+  } else {
+    print('no connection to internet');
   }
 }
 
